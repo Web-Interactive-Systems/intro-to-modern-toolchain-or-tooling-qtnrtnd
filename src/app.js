1b070a46a78d7b5087ccde9c0e21c5b48af8a2f5
Quentin Ratinaud <quentin.ratinaud@gmail.com>
2023-10-04 09:52:58 +0200

    prettier + eslint

--- src/app.js
+++ src/app.js
@@ -1,46 +1,49 @@
-import folio from "./folio.json";
-import { format, parse } from 'date-fns';
-import { fr } from 'date-fns/locale';
+import folio from './folio.json'
+import { format, parse } from 'date-fns'
+import { fr } from 'date-fns/locale'
 
 var test = 2
-console.log(test);
+console.log(test)
 
 const formatDate = (date) => {
-  const dateObj = parse(date, 'yyyy/MM/dd', new Date());
-  return format(dateObj, "EEEE d MMMM yyyy", { locale: fr });
+    const dateObj = parse(date, 'yyyy/MM/dd', new Date())
+    return format(dateObj, 'EEEE d MMMM yyyy', { locale: fr })
 }
 
-const main = document.querySelector('main');
+const main = document.querySelector('main')
 
-for(const sectionName in folio) {
-  const section = document.createElement('section');
-  const title = document.createElement('h2');
+for (const sectionName in folio) {
+    const section = document.createElement('section')
+    const title = document.createElement('h2')
 
-  title.textContent = sectionName;
-  section.appendChild(title);
+    title.textContent = sectionName
+    section.appendChild(title)
 
-  if (Array.isArray(folio[sectionName])) {
-    const ul = document.createElement('ul');
-    const li = document.createElement('li');
+    if (Array.isArray(folio[sectionName])) {
+        const ul = document.createElement('ul')
+        const li = document.createElement('li')
 
-    li.textContent = folio[sectionName].join(', ');
-    ul.appendChild(li);
-    section.appendChild(ul);
+        li.textContent = folio[sectionName].join(', ')
+        ul.appendChild(li)
+        section.appendChild(ul)
+    } else {
+        const ul = document.createElement('ul')
 
-  } else {
-    const ul = document.createElement('ul');
+        for (const itemName in folio[sectionName]) {
+            const li = document.createElement('li')
+            const raw = folio[sectionName][itemName]
 
-    for(const itemName in folio[sectionName]) {
-      const li = document.createElement('li');
-      const raw = folio[sectionName][itemName];
-      
-      li.innerHTML = `<b>${itemName} :</b> ${raw.match(/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/) ? formatDate(raw) : raw}`;
+            li.innerHTML = `<b>${itemName} :</b> ${
+                raw.match(/^[0-9]{4}\/[0-9]{2}\/[0-9]{2}$/)
+                    ? formatDate(raw)
+                    : raw
+            }`
 
-      ul.appendChild(li);
+            ul.appendChild(li)
+        }
+
+        section.appendChild(ul)
     }
 
-    section.appendChild(ul);
-  }
-
-  main.appendChild(section);
+    main.appendChild(section)
 }
